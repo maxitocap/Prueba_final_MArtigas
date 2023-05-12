982db9956915258e4a104a66dff012450a6a7b74
[maxitocap] <artigasmax@gmail.com>
2023-05-11 21:25:42 -0300

    Corrección

--- tests/madisonisland.test.js
+++ tests/madisonisland.test.js
@@ -52,18 +52,17 @@ describe('Interactuar con los anteojos', () => {
   
 
 describe('Test de regresión visual del header', () => {
-  it('Comparación de imágenes con página de WebdriverIO', async () => {
+  it('Comparación de imágenes con página de Madison Island', async () => {
     await browser.url('/');
     await $("#header").waitForDisplayed();
     expect(
-      await browser.checkElement(await $("#header"), "wdio-headerContainer", {
-        /* opciones de configuración para el elemento */
-      }),
-      "Error: la barra de navegación de WebdriverIO no coincide con la original"
+      await browser.checkElement(await $("#header"), "page-header", {}),
+      "Error: el header de Madison Island no coincide con la original"
     ).equal(0);
 
     await browser.url('http://magento-demo.lexiconn.com/?___store=german&___from_store=default');
     await $('#header').waitForDisplayed();
-    expect(await browser.checkElement(await $('#header'), 'wdio-headerContainer', {})).to.not.equal(0);
+    expect(await browser.checkElement(await $('#header'), "page-header", {})).to.not.equal(0);
   })
 })
+
