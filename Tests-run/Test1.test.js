0980efae7163e1e1afd84bc6f0392671ff0da53d
[maxitocap] <artigasmax@gmail.com>
2023-05-21 14:23:30 -0300

    Idioma francés

--- Tests-run/Test1.test.js
+++ Tests-run/Test1.test.js
@@ -1,14 +1,16 @@
+import Index from '../pages/testing.page';
+
 describe('Cambiar idiomas', () => {
   it('Debería cambiar el idioma a francés', async () => {
-    await browser.url('/')
-    const dropDownLanguage = await $('#select-language')
-    await dropDownLanguage.selectByIndex(1)
+    const homePage = new Index();
+    await homePage.changeLanguageToFrench();
   })
 
-  it('Verificar que la página está en francés', async () =>
+  it('Verificar el idioma de la página', async () =>
   {
     const element = await $('select#select-language option[selected="selected"');
     const attr = element.getText().then(text => console.log("El idioma de la página es " + text));
   })
 
 })
+
--- pages/testing.page.js
+++ pages/testing.page.js
@@ -1,6 +1,20 @@
-class index{
+export default class Index{
 
   get languagesBox(){
     return $('#select-language')
+  }
+    async changeLanguageToFrench() {
+    await browser.url('/');
+    await this.languagesBox.selectByIndex(1);
+  }
+
+    async changeLanguageToGerman() {
+    await browser.url('/');
+    await this.languagesBox.selectByIndex(2);
+  }
+
+    async changeLanguageToEnglish() {
+    await browser.url('/');
+    await this.languagesBox.selectByIndex(0);
   }
 }
